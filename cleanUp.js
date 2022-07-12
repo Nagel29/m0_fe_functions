2ed4232a527325b61d0cae595b83af97cc4715b1
Ryan Nagel <RNagel29@gmail.com>
2022-07-11 21:43:22 -0600

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,46 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
-function nameQuestion() {
-  console.log("Hello, what is your name?")
+// EX 1: I added a semi-colon at the end of console.log. Changed function name to start with a verb.
+function askName() {
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+askName()
 
 
-// EX 2:
+// EX 2: Added semi-colon on line 20. Indented lines 20 and 21. Moved the closing } back on line 22.
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
 
-// EX 3:
-func makeFreshPesto(){
+// EX 3: Fixed "func" to "function". Moved closing } from line 34 to 35. Added space between () and { on line 29.
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+//  EX 4: Moved opening { from line 43 to 42. Indented line 42. Moved back (outdented?) line 44. Renamed function to start with verb.
+// Moved period within backticks of console.log. Added code to call function.
+function findAverage(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  console.log(`the average is: ${avg}.`)
+}
 
-  console.log(`the average is: ${avg}`.)
-  }
+findAverage(25, 5)
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,11 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+  * I would say it's currently not a strength or weakness, I'm just doing okay with time management. My main tip would be to expect things to take longer than you would like them to take. Trying to power through something may result in it taking longer than it would if you just took your time.
 - How would you explain to a 5 year old what a function is?
+  * A function is a set of instructions that you tell the computer to remember so that you can later tell it to act out those instructions.
 - Share your understanding of arguments and parameters.
+  * Parameters are variables within the function definition. You can name them whatever you want (along guidelines). You can have multiple variables.
+  * Arguments are actual data that you use when calling the function. This actual data is then applied to the variables that you created when defining the function. Number of arguments must be equal to number of parameters. Arguments can various data types.
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+  * On question 4 of functions.js, was the if statement the best or most efficient way to solve that?
